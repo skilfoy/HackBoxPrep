4747ed47de49d211847524fed7b528427b33d981
skilfoy <20802814+skilfoy@users.noreply.github.com>
2022-04-08 01:45:22 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,6 +27,31 @@ echo '## Building Toolset'
 
 mkdir ~/Documents/Tools
 
+# Manual Installs
+
+echo '[$$] To install Nexpose, enter information, download installer,'
+echo '[$$] run chmod +x on .bin file, then execute .bin with ./'
+xdg-open https://www.rapid7.com/try/nexpose
+
+# apt installs
+
+echo '[$$] Installing Certspotter'
+apt install -y certspotter
+
+echo '[$$] Installing Docker Engine...' 
+apt-get install -y \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  buster stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+chmod a+r /usr/share/keyrings/docker-archive-keyring.gpg
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+
 # Git Clones
 
 echo '[$$] Installing Sn1per...'
@@ -68,6 +93,32 @@ git clone https://github.com/Bandit-HaxUnit/haxunit
 cd haxunit
 python3 -m pip install -r requirements.txt
 python3 main.py --install
+
+echo '[$$] Installing SiteBroker...'
+cd ~/Documents/Tools
+git clone https://github.com/Anon-Exploiter/SiteBroker
+cd SiteBroker
+pip install -r requirements.txt
+
+echo '[$$] Installing Docker Compose V2...'
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+docker compose version
+
+echo '[$$] Installing OWTF...'
+cd ~/Documents/Tools
+git clone https://github.com/owtf/owtf
+cd owtf
+python3 setup.py develop
+make compose
+owtf
+echo '[---->] open `localhost:8009` in the web browser for the OWTF web interface or `owtf --help` for all available commands.'
+
+echo '[$$] Installing LinEnum...'
+cd ~/Documents/Tools
+git clone https://github.com/rebootuser/LinEnum.git
 
 echo '[$$] Installing ***...'
 
