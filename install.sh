c4aba3b66f62718ccc5b44e284f21926dcff04d1
skilfoy <20802814+skilfoy@users.noreply.github.com>
2022-05-05 10:03:19 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,7 +47,7 @@ apt-get install -y \
   curl \
   gnupg \
   lsb-release
-curl -k -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -k -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   buster stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
